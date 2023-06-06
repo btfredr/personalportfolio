db267cb81ffd8a4b34eb071be64f87f366beff95
Fredrik <fredrikf2000@gmail.com>
2023-06-06 11:53:21 +0200

    working on banner functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Navigation from "./components/Navigation";
+import Banner from "./components/Banner";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Banner />
     </div>
   );
 }
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,22 +1,45 @@
-import { Container, Row, Column } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
+import { useState, useEffect } from "react";
+
 const Banner = () => {
+  const [loopNum, setLoop] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+  const [text, setText] = useState("");
+  const period = 2000;
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
   return (
     <section className="banner" id="home">
       <Container>
         <Row className="align-items-center">
           <Col xs={12} md={6} xl={7}>
-            <span className="tagline">Welcome to my portfolio.</span>
             <h1>
-              {`Hi, I'm Fredrik`} <span className="wrap">web developer</span>
+              {`Hi, I'm Fredrik`} <span className="wrap">Web Developer</span>
             </h1>
+            <span className="tagline">Welcome to my portfolio.</span>
             <p>
               Lorem Ipsum is simply dummy text of the printing and typeselling
               industry.
             </p>
             <button onClick={() => console.log("connect")}>
-              Let's Connect
+              Let's Connect <ArrowRightCircle size={25} />
             </button>
+          </Col>
+          <Col xs={12} md={6} xl={7}>
+            <img src={headerImg} alt="Header" />
           </Col>
         </Row>
       </Container>

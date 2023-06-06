abc651c9dcb51967952473ab0ff6a7edbe92e5d0
Fredrik <fredrikf2000@gmail.com>
2023-06-06 12:05:18 +0200

    added typing animation to banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,9 +4,9 @@ import headerImg from "../assets/img/header-img.svg";
 import { useState, useEffect } from "react";
 
 const Banner = () => {
-  const [loopNum, setLoop] = useState(0);
+  const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
-  const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+  const toRotate = ["Frontend Developer", "Web Designer", "UI/UX Designer"];
   const [text, setText] = useState("");
   const period = 2000;
   const [delta, setDelta] = useState(300 - Math.random() * 100);
@@ -21,13 +21,36 @@ const Banner = () => {
     };
   }, [text]);
 
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
+  };
+
   return (
     <section className="banner" id="home">
       <Container>
         <Row className="align-items-center">
           <Col xs={12} md={6} xl={7}>
             <h1>
-              {`Hi, I'm Fredrik`} <span className="wrap">Web Developer</span>
+              {`Hi, I'm Fredrik.`} <span className="wrap">{text}</span>
             </h1>
             <span className="tagline">Welcome to my portfolio.</span>
             <p>

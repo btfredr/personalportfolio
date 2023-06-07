b0f3c5dd5a29527ae6941ed25f84ec967565f61b
Fredrik <fredrikf2000@gmail.com>
2023-06-07 22:01:05 +0200

    working on project card comp

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -54,11 +54,40 @@ const Projects = () => {
               corporis alias recusandae. Maiores veritatis reprehenderit
               incidunt?
             </p>
-            <Nav variant="pills" defaultActiveKey="/home">
-              <Nav.Item>
-                <Nav.Link eventKey="first">Active</Nav.Link>
-              </Nav.Item>
-            </Nav>
+            <Tab.Container id="projects-tabs" defaultActiveKey="first">
+              <Nav variant="pills" defaultActiveKey="/home">
+                <Nav.Item>
+                  <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                </Nav.Item>
+              </Nav>
+              <Tab.Content>
+                <Tab.Pane eventKey="fist">
+                  <Row>
+                    {projects.map((project, index) => {
+                      return <p>{project.title}</p>;
+                    })}
+                  </Row>
+                </Tab.Pane>
+                <Tab.Pane eventKey="second">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Voluptate, amet pariatur iste quasi minus, quos commodi saepe
+                  perspiciatis perferendis dolorem velit voluptatibus culpa
+                  atque quas laborum, aut doloribus tempora a.
+                </Tab.Pane>
+                <Tab.Pane eventKey="third">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+                  dolorem unde natus eius minima possimus veniam asperiores quod
+                  iure, suscipit perferendis neque hic at iusto ratione
+                  inventore reprehenderit sit consectetur!
+                </Tab.Pane>
+              </Tab.Content>
+            </Tab.Container>
           </Col>
         </Row>
       </Container>
